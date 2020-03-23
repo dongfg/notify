ac9ea3c0010412e3496aba0dc62bf5909a5d4b37
dongfg <mail@dongfg.com>
2020-03-23 09:22:03 +0800

    todo

--- cmd/notify/main.go
+++ cmd/notify/main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"github.com/dongfg/notify"
-)
+import "context"
 
 func main() {
-	client := notify.New("ww0cb42f7ec6df90f7", 1000002, "eA-NzUtglvuyBIeJQjdJjwOBe29XbvI5XlO2FpSd9BA")
-	result, err := client.Send(&notify.MessageReceiver{
-		ToUser: "@all",
-	}, notify.Text{Content: "I am content"}, nil)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(result)
-	}
+	context.TODO()
 }
